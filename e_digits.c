744162a338ea82fd2931e48a4534d1bbe573cedf
Sabrina Aravena <saa154@pitt.edu>
2017-04-10 18:43:14 -0400

    Added files.

---
+++ e_digits.c
@@ -0,0 +1,73 @@
+/*************************************************************
+* Project:   /dev/e                                          *
+* File:      e_digits.c                                      *
+* Name:      Sabrina Aravena                                 *
+* Due :      December 11, 2016                               *
+**************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  FILE* dev_file;
+  char *buffer;
+  int f_read;
+  int file_number;
+
+  /* checks for correct number of arguments */
+  if (argc < 3) {
+    printf("Not enouh arguments were entered. TRY AGAIN\n");
+  } else if (argc > 3) {
+    printf("There are too many arguments. TRY AGAIN\n");
+  } else {
+    /* checks for valid arguments */
+    if ((atoi(argv[2])) < (atoi(argv[1]))) {
+      printf("Second argument cannot be larger than the first. TRY AGAIN\n");
+    } else if ((atoi(argv[1])) < 0) {
+      printf("Argument buffer cannot be below 0. TRY AGAIN\n");
+    } else {
+      /* Converts command arguments to integers */
+      int begin = atoi(argv[1]);
+      int end = atoi(argv[2]);
+      /* add the first and last index */
+      int total = begin + end;
+
+      /* add one to index if first argument is zero */
+      if (begin == 0) {
+        total += 1;
+      }
+
+      /* open file to access e_driver */
+      dev_file = fopen("/dev/e", "r");
+      /* Checks if driver exists */
+      if(dev_file == NULL) {
+        perror("This file does not exist. TRY AGAIN\n");
+        return 1;
+      }
+
+      /* Create a buffer to allocate appropriate amount of spcae */
+      buffer = (char*) malloc((total + 1)*sizeof(char));
+      /* Reads in approriate number of bytes (plus one) */
+      file_number = fileno(dev_file);
+      f_read = read(file_number, buffer, (total + 1));
+      /* checks if number of bytes is correct */
+      if(f_read != (total + 1)) {
+        printf("Error reading file. TRY AGAIN\n");
+      }
+
+      /* Prints substring to the terminal */
+      int i;
+      for(i = atoi(argv[1]); i < (total+1); i++) {
+        printf("%c", buffer[i]);
+      }
+      printf("\n");
+
+      /* Free allocated memory and close file */
+      free(buffer);
+      fclose(dev_file);
+    }
+  }
+  return 0;
+} /* end main */
---
+++ e_driver.c
@@ -0,0 +1,73 @@
+/*************************************************************
+* Project:   /dev/e                                          *
+* File:      e_driver.c                                      *
+* Name:      Sabrina Aravena                                 *
+* Due :      December 11, 2016                               *
+**************************************************************/
+
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/miscdevice.h>
+#include <linux/module.h>
+#include <asm/uaccess.h>
+#include "e.h"
+
+/* Figures out buffer and max size and passes in the e from e.h */
+static ssize_t e_driver_read(struct file * file, char * buf, size_t count, loff_t *ppos) {
+	/* calculates sum */
+	int sum = count + (int) *ppos;
+	char buff[sum];
+	/* function implementation in "e.h" */
+	e(buff, sum);
+
+	/* checks that the user has permission to write to the buffer */
+	if (copy_to_user(buf, &buff[*ppos], count))
+		return -EINVAL;
+	/* how much data was written */
+	*ppos = count;
+	return count;
+} /* end ssize_t e_driver_read(struct, char*, size_t, loff_t) */
+
+/* Read op needed */
+static const struct file_operations e_driver_fops = {
+	.owner		= THIS_MODULE,
+	.read		= e_driver_read,
+};
+
+static struct miscdevice e_driver = {
+	/* kernel chooses a random num for MINOR */
+	MISC_DYNAMIC_MINOR,
+	/* Name ourselves /dev/e */
+	"e_driver",
+	/* determines what functions to call */
+	&e_driver_fops
+};
+
+static int __init
+e_driver_init(void) {
+	int ret;
+
+	/*
+	 * Creates the device in the /sys/class/misc directory. */
+	ret = misc_register(&e_driver);
+	if (ret)
+		printk(KERN_ERR
+		       "Unable to register e_driver misc device\n");
+
+	return ret;
+} /* end e_driver_inti() */
+
+module_init(e_driver_init);
+
+static void __exit
+e_driver_exit(void) {
+	misc_deregister(&e_driver);
+} /* end e_driver_exit() */
+
+/* Exit module */
+module_exit(e_driver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Sabrina Aravena <saa154>");
+MODULE_DESCRIPTION("e_driver minimal module");
+MODULE_VERSION("dev");
